82b2f078a6e52b93c48e5f5a2f1cdf227c084e92
AndrewAxen <142675662+AndrewAxen@users.noreply.github.com>
2023-09-28 21:11:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-const myColor = document.querySelector('#exampleColorInput');
-const targetElement = document.body;
-myColor.addEventListener('input', function () {
-  const selectedColor = myColor.value;
-  targetElement.style.backgroundColor = selectedColor;
-});
-
 function reverseString(inputString) {
-  let reversed = '';
+  let reversed = "";
   for (let i = inputString.length - 1; i >= 0; i--) {
     reversed += inputString[i];
   }
   return reversed;
 }
 
+const reverseButton = document.getElementById("reverseButton");
+const inputText = document.getElementById("inputText");
+const result = document.getElementById("result");
 
-const reverseButton = document.getElementById('reverseButton');
-const inputText = document.getElementById('inputText');
-const result = document.getElementById('result');
-
-reverseButton.addEventListener('click', function () {
+reverseButton.addEventListener("click", function () {
   const inputString = inputText.value;
   const reversedString = reverseString(inputString);
   result.textContent = `Reversed string: ${reversedString}`;
 });
+
+/* color */
+
+const myColor = document.querySelector("#exampleColorInput");
+const targetElement = document.body;
+
+myColor.addEventListener("input", function () {
+  const selectedColor = myColor.value;
+  targetElement.style.backgroundColor = selectedColor;
+
+  const contrastTextColor = getContrastColor(selectedColor);
+  targetElement.style.color = contrastTextColor;
+});
+
+function getContrastColor(bgColor) {
+  const r = parseInt(bgColor.slice(1, 3), 16);
+  const g = parseInt(bgColor.slice(3, 5), 16);
+  const b = parseInt(bgColor.slice(5, 7), 16);
+  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+  if (brightness > 128) {
+    return "#000000";
+  } else {
+    return "#FFFFFF";
+  }
+}
